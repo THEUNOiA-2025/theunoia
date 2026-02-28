781c4920813bc0e631bb7ac5ec8bac9a41690470
Seo Ztrategize <seo@ztrategize.com>
2026-02-28 12:55:50 +0530

    Create canonical.js

---
+++ src/components/canonical.js
@@ -0,0 +1,25 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const Canonical = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    let link = document.querySelector("link[rel='canonical']");
+
+    if (!link) {
+      link = document.createElement("link");
+      link.setAttribute("rel", "canonical");
+      document.head.appendChild(link);
+    }
+
+    link.setAttribute(
+      "href",
+      window.location.origin + location.pathname
+    );
+  }, [location]);
+
+  return null;
+};
+
+export default Canonical;
